aea5fc6ceb482d08bb9433c999f196e1f0e74fa7
dx1ded <doskovol1@gmail.com>
2023-07-12 18:23:45 -0600

    Drag n drop attempt with mouse events

--- src/components/Group/Group.jsx
+++ src/components/Group/Group.jsx
@@ -3,15 +3,23 @@ import "./Group.scss"
 
 export const Group = ({ name, children }) => {
   // TODO: set isActive to false by default
-  const [isActive, setIsActive] = useState(true)
+  const [isActive, setIsActive] = useState(
+    JSON.parse(localStorage.getItem(`group-${name}`))
+  )
+
+  const clickHandler = () => {
+    localStorage.setItem(`group-${name}`, JSON.stringify(!isActive))
+    setIsActive(!isActive)
+  }
 
   return (
     <div className={`group ${isActive ? "active" : ""}`}>
       <button
         className="btn-reset group__top"
         aria-label="Open group"
-        onClick={() => setIsActive(!isActive)}
-      >{name}</button>
+        onClick={clickHandler}>
+        {name}
+      </button>
       <div className="group__body">{children}</div>
     </div>
   )
--- src/pages/Kanban/Kanban.jsx
+++ src/pages/Kanban/Kanban.jsx
@@ -1,17 +1,83 @@
+import {useState} from "react"
 import {useAuth} from "../../hooks/useAuth"
 import "./Kanban.scss"
 
-const KanbanItem = ({ title, description }) => {
+function getCoords(element) {
+  const box = element.getBoundingClientRect()
+
+  return {
+    top: box.top + window.scrollY,
+    left: box.left + window.scrollX
+  }
+}
+
+const KanbanItem = ({ title, text, tasks, setTasks }) => {
+  const mouseDownHandler = (event) => {
+    let item = event.target instanceof HTMLLIElement
+      ? event.target
+      : event.target.closest(".kanban-item")
+
+    const coords = getCoords(event.target)
+    const shiftY = event.pageY - coords.top
+    const shiftX = event.pageX - coords.left
+
+    item.style.width = item.getBoundingClientRect().width + "px"
+    item.style.position = "absolute"
+
+    moveAt(event)
+
+    function moveAt(event) {
+      item.style.top = `${event.pageY - shiftY}px`
+      item.style.left = `${event.pageX - shiftX}px`
+    }
+
+    item.ondragstart = null
+    document.onmousemove = (event) => moveAt(event)
+
+    item.onmouseup = () => {
+      document.onmousemove = null
+      item.style.position = "static"
+      item.style.top = "unset"
+      item.style.left = "unset"
+    }
+  }
+
   return (
-    <li className="kanban-item">
+    <li className="kanban-item" onMouseDown={mouseDownHandler}>
       <h4 className="kanban-item__title">{title}</h4>
-      <p className="kanban-item__description">{description}</p>
+      <p className="kanban-item__description">{text}</p>
     </li>
   )
 }
 
 export const Kanban = () => {
-  useAuth()
+  const auth = useAuth()
+  const [tasks, setTasks] = useState({
+    todo: [
+      { title: "Hello", text: "lorem lorem askdnasd, asdksamdk , 213" }
+    ],
+    inProgress: [
+      { title: "Clean my floor", text: "I gotta clean my floor as soon as possible because I'm awaiting for my girlfriend" },
+      { title: "Do some food", text: "Cook a Kyivan cake and some macaroni & cheese" }
+    ],
+    done: [
+      { title: "Wake up at 7 am", text: "Wake up at this time because I need to develop me self-discipline" }
+    ]
+  })
+
+  const addClickHandler = () => {
+    setTasks({
+      ...tasks,
+      todo: [
+        ...tasks.todo,
+        { title: "New task", text: "Task description" }
+      ]
+    })
+  }
+
+  const mouseOverHandler = (event) => {
+    console.log(event)
+  }
 
   return (
     <section className="kanban">
@@ -22,36 +88,41 @@ export const Kanban = () => {
             <span className="material-symbols-outlined" style={{color: "red"}}>radio_button_unchecked</span>
             <h3 className="kanban__name">To-Do</h3>
           </div>
-          <ul className="list-reset kanban__list">
-            <KanbanItem
-              title="Change clothes"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, eum in ipsum itaque iure iusto laboriosam magnam magni minima nam nesciunt nisi odit optio pariatur, perferendis possimus quisquam repellendus rerum unde voluptas."
-            />
-            <KanbanItem
-              title="Change clothes"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, eum in ipsum itaque iure iusto laboriosam magnam magni minima nam nesciunt nisi odit optio pariatur, perferendis possimus quisquam repellendus rerum unde voluptas."
-            />
-            <KanbanItem
-              title="Change clothes"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, eum in ipsum itaque iure iusto laboriosam magnam magni minima nam nesciunt nisi odit optio pariatur, perferendis possimus quisquam repellendus rerum unde voluptas."
-            />
-            <li className="kanban__add">+</li>
+          <ul className="list-reset kanban__list" onMouseOver={mouseOverHandler}>
+            {tasks["todo"].map((task, i) =>
+              <KanbanItem
+                key={i}
+                title={task.title}
+                text={task.text}
+                tasks={tasks}
+                setTasks={setTasks}
+              />
+            )}
+            <li className="kanban__add">
+              <button
+                className="btn-reset"
+                aria-label="Add a new task"
+                onClick={addClickHandler}>
+                +
+              </button>
+            </li>
           </ul>
         </div>
-        <div className="kanban__column">
+        <div className="kanban__column" onMouseOver={mouseOverHandler}>
           <div className="kanban__header">
             <span className="material-symbols-outlined" style={{color: "yellow"}}>radio_button_checked</span>
             <h3 className="kanban__name">In progress</h3>
           </div>
           <ul className="list-reset kanban__list">
-            <KanbanItem
-              title="Change clothes"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, eum in ipsum itaque iure iusto laboriosam magnam magni minima nam nesciunt nisi odit optio pariatur, perferendis possimus quisquam repellendus rerum unde voluptas."
-            />
-            <KanbanItem
-              title="Change clothes"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, eum in ipsum itaque iure iusto laboriosam magnam magni minima nam nesciunt nisi odit optio pariatur, perferendis possimus quisquam repellendus rerum unde voluptas."
-            />
+            {tasks["inProgress"].map((task, i) =>
+              <KanbanItem
+                key={i}
+                title={task.title}
+                text={task.text}
+                tasks={tasks}
+                setTasks={setTasks}
+              />
+            )}
           </ul>
         </div>
         <div className="kanban__column">
@@ -59,11 +130,16 @@ export const Kanban = () => {
             <span className="material-symbols-outlined" style={{color: "green"}}>expand_circle_down</span>
             <h3 className="kanban__name">Done</h3>
           </div>
-          <ul className="list-reset kanban__list">
-            <KanbanItem
-              title="Change clothes"
-              description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, eum in ipsum itaque iure iusto laboriosam magnam magni minima nam nesciunt nisi odit optio pariatur, perferendis possimus quisquam repellendus rerum unde voluptas."
-            />
+          <ul className="list-reset kanban__list" onMouseOver={mouseOverHandler}>
+            {tasks["done"].map((task, i) =>
+              <KanbanItem
+                key={i}
+                title={task.title}
+                text={task.text}
+                tasks={tasks}
+                setTasks={setTasks}
+              />
+            )}
           </ul>
         </div>
       </div>
