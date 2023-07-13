51b73729e3981ddb8165e1788fa2807a16030ea7
dx1ded <doskovol1@gmail.com>
2023-07-13 17:17:06 -0600

    Drag-n-drop done. Page 'Kanban' is also done

--- src/pages/Kanban/Kanban.jsx
+++ src/pages/Kanban/Kanban.jsx
@@ -1,148 +1,225 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd"
+import ContentEditable from "react-contenteditable"
+import {useDebouncedCallback} from "use-debounce"
+import {onAuthStateChanged} from "firebase/auth"
+import {updateDoc} from "firebase/firestore/lite"
+import {getUserDoc} from "../../getUserDoc"
+import {isObjectEmpty} from "../../utils"
+import {useNotification} from "../../components/Notification/useNotification"
+import {useDB} from "../../hooks/useDB"
 import {useAuth} from "../../hooks/useAuth"
+import {Loader} from "../../components/Loader/Loader"
 import "./Kanban.scss"
 
-function getCoords(element) {
-  const box = element.getBoundingClientRect()
-
-  return {
-    top: box.top + window.scrollY,
-    left: box.left + window.scrollX
-  }
-}
-
-const KanbanItem = ({ title, text, tasks, setTasks }) => {
-  const mouseDownHandler = (event) => {
-    let item = event.target instanceof HTMLLIElement
-      ? event.target
-      : event.target.closest(".kanban-item")
-
-    const coords = getCoords(event.target)
-    const shiftY = event.pageY - coords.top
-    const shiftX = event.pageX - coords.left
-
-    item.style.width = item.getBoundingClientRect().width + "px"
-    item.style.position = "absolute"
-
-    moveAt(event)
-
-    function moveAt(event) {
-      item.style.top = `${event.pageY - shiftY}px`
-      item.style.left = `${event.pageX - shiftX}px`
-    }
-
-    item.ondragstart = null
-    document.onmousemove = (event) => moveAt(event)
-
-    item.onmouseup = () => {
-      document.onmousemove = null
-      item.style.position = "static"
-      item.style.top = "unset"
-      item.style.left = "unset"
-    }
-  }
-
-  return (
-    <li className="kanban-item" onMouseDown={mouseDownHandler}>
-      <h4 className="kanban-item__title">{title}</h4>
-      <p className="kanban-item__description">{text}</p>
-    </li>
-  )
-}
+const DEBOUNCED_INTERVAL = 5000
 
 export const Kanban = () => {
   const auth = useAuth()
-  const [tasks, setTasks] = useState({
-    todo: [
-      { title: "Hello", text: "lorem lorem askdnasd, asdksamdk , 213" }
-    ],
-    inProgress: [
-      { title: "Clean my floor", text: "I gotta clean my floor as soon as possible because I'm awaiting for my girlfriend" },
-      { title: "Do some food", text: "Cook a Kyivan cake and some macaroni & cheese" }
-    ],
-    done: [
-      { title: "Wake up at 7 am", text: "Wake up at this time because I need to develop me self-discipline" }
-    ]
-  })
+  const db = useDB()
+  const [api, contextHolder] = useNotification()
+  const [doc, setDoc] = useState({})
+  const [kanban, setKanban] = useState({})
+  const saveDataDebounced = useDebouncedCallback(
+    (kanban) => {
+      updateDoc(doc.ref, { kanban })
+        .then(() => api.add({
+          title: "👍 Saved",
+          text: "Your data has been saved successfully!",
+          delay: DEBOUNCED_INTERVAL
+        }))
+      },
+    DEBOUNCED_INTERVAL
+  )
 
-  const addClickHandler = () => {
-    setTasks({
-      ...tasks,
-      todo: [
-        ...tasks.todo,
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, async (user) => {
+      if (!user?.email) return
+
+      try {
+        const doc = await getUserDoc(db, user.email)
+
+        setDoc(doc)
+        setKanban(doc.data().kanban)
+      } catch (e) {
+        console.error(e)
+      }
+    })
+
+    return listen
+  }, [auth, db])
+
+  const markupData = [
+    {
+      name: "To-Do",
+      icon: "radio_button_unchecked",
+      iconColor: "red",
+      newButton: true
+    },
+    {
+      name: "In progress",
+      icon: "radio_button_checked",
+      iconColor: "yellow"
+    },
+    {
+      name: "Done",
+      icon: "expand_circle_down",
+      iconColor: "green"
+    }
+  ]
+
+  const addNewTask = () => {
+    const newState = {
+      ...kanban,
+      "To-Do": [
+        ...kanban["To-Do"],
         { title: "New task", text: "Task description" }
       ]
-    })
+    }
+
+    setKanban(newState)
+    saveDataDebounced(newState)
+  }
+  const changeTask = (props, event, type) => {
+    const [columnName, index] = props["data-rbd-draggable-id"].split("/")
+
+    const newState = {
+      ...kanban,
+      [columnName]: kanban[columnName].map(
+        (item, i) => i === +index
+          ? { ...kanban[columnName][index], [type]: event.target.value }
+          : item
+      )
+    }
+
+    setKanban(newState)
+    saveDataDebounced(newState)
+  }
+  const deleteTask = (props) => {
+    const [columnName, index] = props["data-rbd-draggable-id"].split("/")
+
+    const newState = {
+      ...kanban,
+      [columnName]: kanban[columnName].filter((_, i) => i !== +index)
+    }
+
+    setKanban(newState)
+    saveDataDebounced(newState)
+  }
+  const handleDragDrop = (results) => {
+    const { source, destination } = results
+
+    if (
+      !destination || (
+        source.droppableId === destination.droppableId &&
+        source.index === destination.index
+      )
+    ) return
+
+    const [
+      { droppableId: sId, index: sIndex },
+      { droppableId: dId, index: dIndex }
+    ] = [source, destination]
+
+    const filteredCol = kanban[dId].filter((_, i) => i !== sIndex)
+
+    let newState = {
+      ...kanban,
+      [sId]: kanban[sId].filter((_, i) => i !== sIndex),
+      [dId]: [
+        ...(sId === dId ? filteredCol : kanban[dId]).slice(0, dIndex),
+        kanban[sId][sIndex],
+        ...(sId === dId ? filteredCol : kanban[dId]).slice(dIndex)
+      ]
+    }
+
+    setKanban(newState)
+    saveDataDebounced(newState)
   }
 
-  const mouseOverHandler = (event) => {
-    console.log(event)
+  if (isObjectEmpty(kanban)) {
+    return <Loader />
   }
 
   return (
-    <section className="kanban">
-      <h2 className="title--lg kanban__title">Kanban</h2>
-      <div className="kanban__container">
-        <div className="kanban__column">
-          <div className="kanban__header">
-            <span className="material-symbols-outlined" style={{color: "red"}}>radio_button_unchecked</span>
-            <h3 className="kanban__name">To-Do</h3>
-          </div>
-          <ul className="list-reset kanban__list" onMouseOver={mouseOverHandler}>
-            {tasks["todo"].map((task, i) =>
-              <KanbanItem
-                key={i}
-                title={task.title}
-                text={task.text}
-                tasks={tasks}
-                setTasks={setTasks}
-              />
+    <>
+      {contextHolder}
+      <section className="kanban">
+        <h2 className="title--lg kanban__title">Kanban</h2>
+        <DragDropContext onDragEnd={handleDragDrop}>
+          <div className="kanban__container">
+            {markupData.map((data) =>
+              <div className="kanban__column" key={data.name}>
+                <div className="kanban__header">
+                  <span
+                    className="material-symbols-outlined"
+                    style={{color: data.iconColor}}>
+                    {data.icon}
+                  </span>
+                  <h3 className="kanban__name">{data.name}</h3>
+                </div>
+                <Droppable droppableId={data.name} type="group">
+                  {(provided) => (
+                    <ul
+                      className="list-reset kanban__list"
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                    >
+                      {kanban[data.name].map((task, i) =>
+                        <Draggable draggableId={`${data.name}/${i}`} key={i} index={i}>
+                          {(provided) => (
+                            <li
+                              className="kanban-item"
+                              {...provided.draggableProps}
+                              ref={provided.innerRef}
+                            >
+                              <div className="kanban-item__actions">
+                                <span
+                                  className="material-symbols-outlined kanban-item__delete"
+                                  onClick={() => deleteTask(provided.draggableProps)}>
+                                  delete
+                                </span>
+                                <span
+                                  className="material-symbols-outlined kanban-item__drag"
+                                  {...provided.dragHandleProps}>
+                                  drag_handle
+                                </span>
+                              </div>
+                              <ContentEditable
+                                className="kanban-item__title"
+                                tagName="h4"
+                                html={task.title}
+                                onChange={(event) => changeTask(provided.draggableProps, event, "title")}
+                              />
+                              <ContentEditable
+                                className="kanban-item__description"
+                                tagName="p"
+                                html={task.text}
+                                onChange={(event) => changeTask(provided.draggableProps, event, "text")}
+                              />
+                            </li>
+                          )}
+                        </Draggable>
+                      )}
+                      {data.newButton &&
+                        <li className="kanban__add">
+                          <button
+                            className="btn-reset"
+                            aria-label="Add a new task"
+                            onClick={addNewTask}>
+                            +
+                          </button>
+                        </li>
+                      }
+                      {provided.placeholder}
+                    </ul>
+                  )}
+                </Droppable>
+              </div>
             )}
-            <li className="kanban__add">
-              <button
-                className="btn-reset"
-                aria-label="Add a new task"
-                onClick={addClickHandler}>
-                +
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div className="kanban__column" onMouseOver={mouseOverHandler}>
-          <div className="kanban__header">
-            <span className="material-symbols-outlined" style={{color: "yellow"}}>radio_button_checked</span>
-            <h3 className="kanban__name">In progress</h3>
           </div>
-          <ul className="list-reset kanban__list">
-            {tasks["inProgress"].map((task, i) =>
-              <KanbanItem
-                key={i}
-                title={task.title}
-                text={task.text}
-                tasks={tasks}
-                setTasks={setTasks}
-              />
-            )}
-          </ul>
-        </div>
-        <div className="kanban__column">
-          <div className="kanban__header">
-            <span className="material-symbols-outlined" style={{color: "green"}}>expand_circle_down</span>
-            <h3 className="kanban__name">Done</h3>
-          </div>
-          <ul className="list-reset kanban__list" onMouseOver={mouseOverHandler}>
-            {tasks["done"].map((task, i) =>
-              <KanbanItem
-                key={i}
-                title={task.title}
-                text={task.text}
-                tasks={tasks}
-                setTasks={setTasks}
-              />
-            )}
-          </ul>
-        </div>
-      </div>
-    </section>
+        </DragDropContext>
+      </section>
+    </>
   )
 }
