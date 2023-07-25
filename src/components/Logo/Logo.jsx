4ba6859f70cd232fce57ba02bd250012b9a9f718
dx1ded <doskovol1@gmail.com>
2023-07-24 19:34:44 -0600

    Images bug fixed. Also fixed a bug with kanban on mobile devices

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import {NavLink} from "react-router-dom"
+import Rocket from "./rocket.svg"
 import "./Logo.scss"
 
 export const Logo = () => (
   <NavLink to="/" className="logo">
-    <img className="logo__image" src="./images/rocket.svg" alt="" aria-hidden="true" />
+    <img className="logo__image" src={Rocket} alt="" aria-hidden="true" />
     <h2 className="logo__name">
       to
       <span>do</span>
